f61c58d512c92819f83c415c6a0c6701c16c736f
seenusneha4 <seenaannmathew6@gmail.com>
2022-05-19 22:00:16 +0530

    login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Login = () => {
+    var [username,setusername]=useState("")
+    var [password,setpassword]=useState("")
+    const subdata=()=>{
+        const data={"username":username,"password":password}
+        console.log(data)
+    }
   return (
     <div>
         <div className="container">
@@ -9,17 +15,18 @@ const Login = () => {
         <div className="row g-3">
             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
             <label for="" className="form-label">Username</label>
-            <input type="text" className="form-control"/>
+            <input onChange={(a)=>{setusername(a.target.value)}} type="text" className="form-control"/>
             </div>
             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                  <label for="" className="form-label">Password</label>
-                 <input type="text" className="form-control"/>
+                 <input onChange={(a)=>{setpassword(a.target.value)}} type="password" name="" id="" class="form-control"></input>
+
             </div>
              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-               <button className="btn btn-success">Login</button>
+               <button onClick={subdata} className="btn btn-success">Login</button>
             </div>
             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-              <Link to="/">New user click here</Link>
+              <Link to="/register">New user click here</Link>
                 
                 
            </div>
